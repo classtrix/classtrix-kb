e3773a7d7888d61feafaaae011c1a71d5271afae
Jaj Kumar <jaj009@gmail.com>
2026-04-16 17:45:04 +0530

    chore: update site URL and base path for deployment; add CNAME file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://classtrix.github.io',
+  url: 'https://kb.classtrix.in',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/classtrix-kb',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
