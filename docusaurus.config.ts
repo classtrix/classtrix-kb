f0d78c31385621b762138204593d1dcd3eed5ada
Jaj Kumar <jaj009@gmail.com>
2026-04-17 03:23:36 +0530

    fix: update edit URL in docusaurus configuration for correct GitHub link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/classtrix/classtrix-kb/tree/main/',
+          editUrl: 'https://github.dev/classtrix/classtrix-kb/blob/main/',
         },
         blog: {
           showReadingTime: true,
